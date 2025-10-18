5fadfe6510f5dfeb80bb59e2cbd480903f09cd43
felipee123123123123123123 <huciuhuciu@gmail.com>
2025-10-18 13:40:22 +0200

    Create zad4.cpp

---
+++ zad4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	double cena_b, odl, spal, koszt;
+
+	cena_b = 7;
+	odl = 335;
+	spal = 9;
+	koszt = odl * cena_b * spal/100;
+
+	cout << "koszt przejazdu:" << koszt; "zl";
+
+
+
+
+		return 0;
+}
