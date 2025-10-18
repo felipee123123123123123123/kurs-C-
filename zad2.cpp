91967e381858862f65f845ef874a5d3d940ad91d
felipee123123123123123123 <huciuhuciu@gmail.com>
2025-10-18 12:32:40 +0200

    Create zad2.cpp

---
+++ zad2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	double a, h, pole;
+
+	cout << "podaj a:"<<endl;
+	cin >> a;
+	cout << "podaj h:"<<endl;
+	cin >> h;
+	pole = (a * h) / 2;
+
+	cout<<"pole"<<pole;
+
+	return 0;
+	
+}
