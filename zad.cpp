195a0d45af9f1e6d8d97e1128090a812f7cf2d4c
felipee123123123123123123 <huciuhuciu@gmail.com>
2025-10-18 12:46:26 +0200

    Create zad.cpp

---
+++ zad.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	double r, pi, obwod, pole;
+
+	cout << "Podaj r:" << endl;
+	cin >> r;
+	pi = 3.14;
+
+	obwod = 2 * pi * r;
+	pole = pi * (r * r);
+
+	cout << "pole:" << pole << endl;
+	cout << "obwod:" << obwod;
+
+	return 0;
+}
