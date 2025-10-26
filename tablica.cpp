2f2abb9d1f4b3d93584c0cfaacd1277e4a027bcc
felipee123123123123123123 <huciuhuciu@gmail.com>
2025-10-26 12:11:24 +0100

    Update tablica.cpp

--- tablica.cpp
+++ tablica.cpp
@@ -81,3 +81,40 @@ int main() {
 	}
 	return 0;
 }
+-------------------------------------------------------------------------------------
+
+	#include<iostream>      
+#include<cstring>
+#include<cmath>
+#include<cctype>
+#include<iomanip>
+
+
+
+using namespace std;
+
+
+int main() {
+	float tab[5];
+	float sre;
+	float suma=0;
+	int n=0;
+	cout << "daj liczbe do tabeli:" << endl;
+	for (int a = 0; a < 5; ++a) {
+
+		cin >> tab[a];
+		if (tab[a] > 0) {
+			n = n + 1;
+			suma = tab[a] + suma;
+		}
+	}
+	if (suma == 0) {
+		cout << "nie ma dodatnich liczb.";
+	}
+	else {
+		sre = suma / n;
+		cout << "średnia z dodatnich liczb wynosi: " << sre << endl;
+	}
+	
+	return 0;
+}
