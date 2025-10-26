792e2cebb9e4f69cbfacac345ee1ad2181acc4cb
felipee123123123123123123 <huciuhuciu@gmail.com>
2025-10-26 12:41:01 +0100

    Update tablica.cpp

--- tablica.cpp
+++ tablica.cpp
@@ -118,3 +118,46 @@ int main() {
 	
 	return 0;
 }
+--------------------------------------------------------------------------------
+
+	#include<iostream>      
+#include<cstring>
+#include<cmath>
+#include<cctype>
+#include<iomanip>
+
+
+
+using namespace std;
+
+
+int main() {
+    int tab[5];
+    int ppl[5];
+
+    cout << "podaj liczby do pierwszej tabeli:" << endl;
+    for (int a = 0; a < 5; ++a) {
+        cin >> tab[a];
+    }
+
+    cout << "podaj liczby do drugiej tabeli:" << endl;
+    for (int i = 0; i < 5; ++i) {
+        cin >> ppl[i];
+    }
+
+    bool takieSame = true;
+
+    for (int c = 0; c < 5; ++c) {
+        if (tab[c] != ppl[c]) {
+            takieSame = false;
+            break; // nie ma sensu dalej sprawdzać
+        }
+    }
+
+    if (takieSame)
+        cout << "Tabele sa takie same." << endl;
+    else
+        cout << "Tabele sie roznia." << endl;
+
+    return 0;
+}
